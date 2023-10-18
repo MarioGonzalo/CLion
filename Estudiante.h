f3762f911ceafb676f6724a96a17f70e2e382d3b
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2023-10-18 22:34:00 +0200

    Add files via upload

---
+++ Estudiante.h
@@ -0,0 +1,21 @@
+//
+// Created by magol on 17/10/2023.
+//
+
+#ifndef UNTITLED_ESTUDIANTE_H
+#define UNTITLED_ESTUDIANTE_H
+
+#include <string>
+#include "Persona.h"
+
+class Estudiante : public Persona {
+    std::string grado;
+
+public:
+    void setGrado(const std::string &grado);
+    std::string getGrado();
+    void mostrarDetalles();
+};
+
+
+#endif //UNTITLED_ESTUDIANTE_H
---
+++ nuevo.cpp
@@ -0,0 +1,3 @@
+//
+// Created by magol on 18/10/2023.
+//
